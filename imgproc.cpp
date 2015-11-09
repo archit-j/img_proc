b13740f5d5afa5b58d1027362f8573a45fd77b70
archit-j <amiarchit@gmail.com>
2015-11-09 17:10:10 +0530

    Update imgproc.cpp

--- imgproc.cpp
+++ imgproc.cpp
@@ -18,14 +18,14 @@ int iHighS = 255;
 int iLowV = 12;
 int iHighV = 106;
 
-int iLowH_line = 25;
-int iHighH_line = 51;
+int iLowH_line = 27;
+int iHighH_line = 62;
 
-int iLowS_line = 135; 
-int iHighS_line = 255;
+int iLowS_line = 100; 
+int iHighS_line = 204;
 
-int iLowV_line = 12;
-int iHighV_line = 106;
+int iLowV_line = 27;
+int iHighV_line = 121;
 
 
 int smooth=1;
@@ -38,87 +38,115 @@ int countour_field_switch=0;
 int field_generate=0;
 int region_of_interest_switch=0;
 int canny_switch=0;
-int canny_thresh=50;
 int blur_median=3;
+
+int canny_thresh=50;
+int canny_ratio=3;
+int canny_kernel=1;
 
 
 int amount_of_white_neighbours=2;
 int resolution_of_digestion=12;
 int radius_of_major_nodes=5;
+int nearest_neighbour_scope=15;
+int calc_cij_switch=0;
+int node_draw_switch=0;
+int line_traversal_scope=2;
+int Tolerance=5;
 
-void calcCij(Mat &image, vector<vector<int> > &cij)
+int line_cij=0;
+int Add_line=0;
+
+void calcCij(Mat &image, vector<vector<int> > &cij) //cij implies information about x's and t's
 {
-  int IMAGE_WIDTH = image.cols;
-  int IMAGE_HEIGHT = image.rows;
-  cij.clear();
-  cij.resize(IMAGE_WIDTH, vector<int>(IMAGE_HEIGHT, INF));
-  vector<vector<int> > whiteCount(vector<vector<int> >(IMAGE_WIDTH, vector<int>(IMAGE_HEIGHT, 0)));
-  vector<vector<int> > digest(vector<vector<int> >(IMAGE_WIDTH, vector<int>(IMAGE_HEIGHT, 0)));
-  vector<vector<int> > Order(vector<vector<int> >(IMAGE_WIDTH, vector<int>(IMAGE_HEIGHT, 0)));
-  vector<vector<int> > parent(vector<vector<int> >(IMAGE_WIDTH, vector<int>(IMAGE_HEIGHT, 0)));
-  for (int i = resolution_of_whitecount; i < IMAGE_WIDTH - resolution_of_whitecount; ++i)
+  
+  Size size=image.size();
+  vector<vector<int> > whiteCount(vector<vector<int> >(size.width, vector<int>(size.height, INF)));
+  vector<vector<int> > Digest(vector<vector<int> >(size.width, vector<int>(size.height, INF)));
+ // vector<vector<int> > cij(vector<vector<int> >(size.width, vector<int>(size.height, INF)));
+//  vector<vector<int> > cij(vector<vector<int> >(size.width, vector<int>(size.height, INF)));
+
+  
+  for(int j= resolution_of_whitecount   ;j<size.height- resolution_of_whitecount   ;j++)
   {
-    for (int j = resolution_of_whitecount; j < IMAGE_HEIGHT-resolution_of_whitecount; ++j)
+    for(int i= resolution_of_whitecount   ;i<size.width- resolution_of_whitecount   ;i++)
     {
-      for (int x = -1*resolution_of_whitecount; x < resolution_of_whitecount+1; ++x)
+      whiteCount[i][j]=0;
+
+      for(int xx=-1*resolution_of_whitecount;xx<resolution_of_whitecount+1;xx++)
       {
-        for (int y = -1*resolution_of_whitecount; y < resolution_of_whitecount+1; ++y)
-        {  Scalar intensity = image.at<uchar>(y+j, x+i);
-          if(intensity.val[0]!=0)
-          whiteCount[i][j]++;
+        for(int yy=-1*resolution_of_whitecount;yy<resolution_of_whitecount+1;yy++)
+        {
+          Point pt(i+xx,j+yy);
+          Vec3b intensity = image.at<Vec3b>(j+yy, i+xx);
+          uchar blue = intensity.val[0];
+          uchar green = intensity.val[1];
+          uchar red = intensity.val[2];
+          if(blue>0||green>0||red>0)
+          {
+            image.at<Vec3b>(j,i)[0]=0;
+            image.at<Vec3b>(j,i)[1]=0;
+            image.at<Vec3b>(j,i)[2]=0;
+            whiteCount[i][j]++;
+          }
         }
       }
-      digest[i][j]=0;
+
+      Digest[i][j]=0;
+
       if(whiteCount[i][j]==amount_of_white_neighbours+1)
       {
-        digest[i][j]=1;
-        parent[i][j]=1;
+        Digest[i][j]=1;
         cij[i][j]=1;
+
       }
       if(whiteCount[i][j]==amount_of_white_neighbours+2)
       {
-        digest[i][j]=3;
-        parent[i][j]=1;
+        Digest[i][j]=3;
         cij[i][j]=1;
+
       }
       if(whiteCount[i][j]==amount_of_white_neighbours+3)
       {
-        digest[i][j]=9;
-        parent[i][j]=1;
+        Digest[i][j]=9;
         cij[i][j]=1;
+
       }
+      //
+      //
     }
   }
-  for (int i = resolution_of_digestion; i < IMAGE_WIDTH - resolution_of_digestion; ++i)
+
+  for(int j= resolution_of_digestion ;j<size.height- resolution_of_digestion ;j++)
   {
-    for (int j = resolution_of_digestion; j < IMAGE_HEIGHT - resolution_of_digestion; ++j)
+    for(int i= resolution_of_digestion ;i<size.width- resolution_of_digestion ;i++)
     {
-      if(whiteCount[i][j] == 0)
-        continue;
+      if(whiteCount[i][j] == 0||Digest[i][j]==0)
+          continue;
       int max_digest=0,maxi=0,maxj=0;
-      if(digest[i][j]>=1)
-      { max_digest=digest[i][j];
+      if(Digest[i][j]>0)
+      { max_digest=Digest[i][j];
         maxj=j;
         maxi=i;
-         int digested_count = 0;
-        for (int x = -1*resolution_of_digestion; x < resolution_of_digestion+1; ++x)
-        {   
-          for (int y = -1*resolution_of_digestion; y < resolution_of_digestion+1; ++y)
-          {  
-            if(digest[i+x][j+y]>max_digest)
+        int digested_count = 0;
+        for(int xx=-1*resolution_of_digestion ; xx <resolution_of_digestion + 1 ; xx++)
+        {
+          for(int yy=-1*resolution_of_digestion ; yy <resolution_of_digestion + 1 ; yy++)
+          {
+           if(Digest[i+xx][j+yy]>max_digest)
             { 
-              max_digest=digest[i+x][j+y];
-              maxi=i+x;
-              maxj=j+y;
+              max_digest=Digest[i+xx][j+yy];
+              maxi=i+xx;
+              maxj=j+yy;
             }
             else 
             {
-              digested_count+=digest[i+x][j+y];
-              digest[i+x][j+y]=0;
+              Digest[i+xx][j+yy]=0;
+              digested_count++;
             }
           }
         }
-        digest[maxi][maxj]+=digested_count;
+        Digest[maxi][maxj]+=digested_count;
         cij[maxi][maxj]=2;
       }
     }
@@ -126,20 +154,116 @@ void calcCij(Mat &image, vector<vector<int> > &cij)
 }
 
 void createImageNodes(Mat &image, vector<vector<int> > &cij)
-{
-  for (int i = 0; i < cij.size(); ++i)
   {
-    for (int j = 0; j < cij[0].size(); ++j)
+    for (int i = 0; i < cij.size(); ++i)
+  {
+    for (int j = 0; j < cij[5].size(); ++j)
     { 
       
-      { j/=3;
+      { 
         if(cij[i][j] == 1)
         circle(image, Point(i, j), 3, CV_RGB(255, 0, 0));
         if(cij[i][j] ==2)
-        circle(image, Point(i, j), radius_of_major_nodes, CV_RGB(0, 255, 0));
+        circle(image, Point(i,j), radius_of_major_nodes, CV_RGB(0, 255, 0));
       }
     }
   }
+}
+void createImageEdges(Mat image, vector<vector<int> > &cij)
+{ ///loop to traverse the image
+  Size size=image.size();
+  int IMAGE_WIDTH = size.width;
+  int IMAGE_HEIGHT = size.height;
+  vector<vector<int> > order;
+  order=cij;
+
+  for (int i = 0; i < cij.size(); ++i)
+    {
+      for (int j = 0; j < cij[0].size(); ++j)
+      {
+       order[i][j]=0;
+      }
+    }
+
+  for (int i = 0+resolution_of_digestion + nearest_neighbour_scope  ; i < cij.size()-resolution_of_digestion - nearest_neighbour_scope  ; ++i)
+  {
+    for (int j = 0+resolution_of_digestion + nearest_neighbour_scope  ; j < cij[0].size()-resolution_of_digestion - nearest_neighbour_scope  ; ++j)
+    { 
+
+
+
+             ///consider only the major nodes
+      if(cij[i][j]==2)
+      {   ////look for major nodes in the neighbourhood
+        for (int x = -1*resolution_of_digestion - nearest_neighbour_scope  ; x < resolution_of_digestion+1 + nearest_neighbour_scope  ; x++)
+        {  
+          for (int y = -1*resolution_of_digestion - nearest_neighbour_scope  ; y < resolution_of_digestion+1 + nearest_neighbour_scope  ; ++y)
+          {   ///conditional to locate a major node neighbour 
+            if(cij[i+x][j+y]==2)
+            {   Point p1,p2;
+              p1.x=i;
+              p1.y=j;
+              p2.x=i+(x);
+              p2.y=j+y;
+              float filled_ratio=0;
+              int count_of_minor_nodes=0;
+              LineIterator it(image, p1, p2, 8);
+
+
+              ///loop to traverse the line joning the node and its neighbour
+              for(int ii = 0; ii < it.count; ii++, ++it)
+              {   ///loop to search the neighbourhood of each of the points of the line 
+
+                for (int xx = -1*line_traversal_scope; xx < line_traversal_scope+1; xx++)
+                {  
+                  for (int yy = -1*line_traversal_scope; yy < line_traversal_scope+1; ++yy)
+                  ///check whether the point of the line is a minor passing node
+                  {
+                    Point line_point =   it.pos();
+                  
+                      int search_point_x = line_point.x+xx;
+                      int search_point_y = line_point.y+yy;
+          
+                        if(cij[search_point_x][search_point_y]==1)
+                        {
+                          count_of_minor_nodes++;
+                        }
+                  }
+                }
+                filled_ratio=(count_of_minor_nodes)/(it.count);
+                RNG& rng = theRNG();
+                if(filled_ratio>=Tolerance)
+                {  if(p1==p2) break;
+                  line(image, p1, p2, CV_RGB(rng.uniform(254,255),rng.uniform(254,255),rng.uniform(254,255)),rng.uniform(radius_of_major_nodes,radius_of_major_nodes), 8);
+                  count_of_minor_nodes=0;
+                  order[p1.x][p1.y]++;
+                  order[p2.x][p2.y]++;
+                }
+              }
+              
+            }
+          }
+        }
+      }
+    }
+  }
+  
+
+  for (int i = 0; i < cij.size(); ++i)
+    {
+      for (int j = 0; j < cij[0].size(); ++j)
+      { Point pt(i,j);
+       switch(order[i][j])
+       {
+        case 0: break;
+        case 1:  circle(image, cvPoint(i, j),radius_of_major_nodes, CV_RGB(255,255, 0),-1); break;
+        case 2: circle(image, cvPoint(i, j),radius_of_major_nodes, CV_RGB(0, 255, 0),-1);  break;
+        case 3: circle(image, cvPoint(i, j),radius_of_major_nodes, CV_RGB(0, 0, 255),-1);  break;
+        default : circle(image, cvPoint(i, j),radius_of_major_nodes, CV_RGB(250, 125, 255),-1); break ;
+       }
+      }
+    }
+
 }
 
 void init_flow()
@@ -147,10 +271,19 @@ void init_flow()
   namedWindow("Flow", CV_WINDOW_AUTOSIZE); //create a window called "Control"
   createTrackbar("field_generate", "Flow", &field_generate, 1);
     createTrackbar("countour_field_switch", "Flow", &countour_field_switch, 1);
-  createTrackbar("canny_thresh", "Flow", &canny_thresh, 255);
+  
   createTrackbar("region_of_interest_switch", "Flow", &region_of_interest_switch, 1);
   createTrackbar("canny", "Flow", &canny_switch, 1);
+    createTrackbar("calc_cij_switch", "Flow", &calc_cij_switch, 1);
+
+  createTrackbar("node_draw_switch", "Flow", &node_draw_switch, 1);
+   namedWindow("Canny", CV_WINDOW_AUTOSIZE);
+   createTrackbar("canny_thresh", "Canny", &canny_thresh, 500);
+   createTrackbar("canny_ratio", "Canny", &canny_ratio, 5);
+   createTrackbar("canny_kernel", "Canny", &canny_kernel, 10);
 }
+
+
 
 
 void init_grass_interface()
@@ -193,6 +326,10 @@ void init_line_interface()
   createTrackbar("smooth", "Control_line", &smooth_line, 1);
   createTrackbar("smooth_size", "Control_line", &smooth_size_line, 5);
 
+  createTrackbar("Add_line", "Control_line", &Add_line, 5);
+  createTrackbar("line_cij", "Control_line", &line_cij, 5);
+ // createTrackbar("smooth_size", "Control_line", &smooth_size_line, 5);
+
 }
 void init_node_control()
 {
@@ -200,11 +337,12 @@ void init_node_control()
   createTrackbar( "amount_of_white_neighbours", "Node_Control", &amount_of_white_neighbours,9);
   //createTrackbar( "circle", "Node_Control", &b,255);
   createTrackbar( "digesting reso", "Node_Control", &resolution_of_digestion,255);
-   //createTrackbar( "line", "Node_Control", &e,255, set_thresh );
-  //createTrackbar( "Edge Tolerance", "Node_Control", &Tolerance,40);
-  //createTrackbar( "line_traversal_scope", "Node_Control", &lts,2);
+  createTrackbar( "nearest_neighbour_scope", "Node_Control", &nearest_neighbour_scope,255);
+  createTrackbar( "Edge Tolerance", "Node_Control", &Tolerance,40);
+  createTrackbar( "line_traversal_scope", "Node_Control", &line_traversal_scope,2);
   createTrackbar( "radius_of_major_nodes", "Node_Control", &radius_of_major_nodes,25);
 }
+
 
 void contourify(Mat &imgHull,bool max=true)
 {
@@ -303,7 +441,7 @@ void canny(Mat &src_gray)
   vector<Vec4i> hierarchy;
 
   /// Detect edges using canny
-  Canny( src_gray, canny_output, canny_thresh, canny_thresh*2, 3 );
+  Canny( src_gray, canny_output, canny_thresh, canny_thresh*canny_ratio, canny_kernel*2+1);
   /// Find contours
   findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
@@ -318,6 +456,27 @@ void canny(Mat &src_gray)
   /// Show in a window
   src_gray=drawing;
 }
+void thin(Mat &img)
+{
+  img.convertTo(img,CV_8UC1);
+  cv::Mat skel(img.size(), CV_8UC1, cv::Scalar(0));
+  cv::Mat temp(img.size(), CV_8UC1);
+  cv::Mat element = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
+  bool done;
+  do
+  {
+    cv::morphologyEx(img, temp, cv::MORPH_OPEN, element);
+    cv::bitwise_not(temp, temp);
+    cv::bitwise_and(img, temp, temp);
+    cv::bitwise_or(skel, temp, skel);
+    cv::erode(img, img, element);
+   
+    double max;
+    cv::minMaxLoc(img, 0, &max);
+    done = (max == 0);
+  } while (!done);
+  img=skel;
+}
 int main( int argc, char** argv )
 {
   if(argc<2)
@@ -331,6 +490,8 @@ int main( int argc, char** argv )
   init_flow();
   init_node_control();
 
+    
+
   while (true)
   {
     Mat imgOriginal;
@@ -340,7 +501,13 @@ int main( int argc, char** argv )
     imgOriginal=imread(argv[1]);
     Mat imgField=imread(argv[1]);
     Mat imgField2;
-    vector<vector<int> > cij;
+
+    int IMAGE_WIDTH = imgOriginal.cols;
+     int IMAGE_HEIGHT = imgOriginal.rows;
+
+
+    vector<vector<int> > cij(vector<vector<int> >(IMAGE_WIDTH, vector<int>(IMAGE_HEIGHT, INF)));
+
 
     HSV_threshold(imgOriginal,imgThresholded);  
     
@@ -356,29 +523,70 @@ int main( int argc, char** argv )
     {   
       infield(imgField);
     }
+
+
     imgField2=imgField.clone();
+
+
     HSV_threshold_line(imgField2,imgThresholded_line);
+     thin(imgThresholded_line);
+
+
      if(region_of_interest_switch&&field_generate)
     {
       if(canny_switch)
       {
         canny(imgField);
+        if(countour_field_switch)
+        {
+              //morphological opening (removes small objects from the foreground)
+        erode(imgField, imgField, getStructuringElement(MORPH_ELLIPSE, Size(smooth_size, smooth_size)));
+        dilate( imgField, imgField, getStructuringElement(MORPH_ELLIPSE, Size(smooth_size, smooth_size)) ); 
+
+        //morphological closing (removes small holes from the foreground)
+     //   dilate( imgField, imgField, getStructuringElement(MORPH_ELLIPSE, Size(smooth_size, smooth_size)) ); 
+      //  erode(imgField, imgField, getStructuringElement(MORPH_ELLIPSE, Size(smooth_size, smooth_size)) );
+        }
+
         canny(imgThresholded_line);
-        calcCij(imgField, cij);
-        createImageNodes(imgField, cij);
+        if(calc_cij_switch)
+        {
+          calcCij(imgField, cij);
 
+          //printf("yolo yolo");
+
+          if(node_draw_switch)
+          {
+            createImageNodes(imgField, cij);
+            createImageEdges(imgField,cij);
+           // printf("nigga nigga");
+          }
+        }
       }
-
     }
-    if(countour_field_switch)
+
+    if(countour_field_switch&&!canny_switch)
     {
       contourify(imgThresholded_line,false);
     }
-      imgThresholded_line.convertTo(imgThresholded_line,CV_8UC3);
+     
     imgOriginal.convertTo(imgOriginal,CV_8UC3);
     imgField.convertTo(imgField,CV_8UC3);
     imgHull.convertTo(imgHull,CV_8UC3);
     imgOriginal+=imgField;
+    if(line_cij)
+      {
+       
+      //  imgThresholded_line.convertTo(imgThresholded_line,CV_8UC3);
+        calcCij(imgThresholded_line,cij);
+        createImageNodes(imgThresholded_line,cij);
+        createImageEdges(imgThresholded_line,cij);
+      }
+    if(Add_line)
+    {
+      imgThresholded_line.convertTo(imgThresholded_line,CV_8UC3);
+      imgOriginal+=imgThresholded_line;
+    }
 
 
   //  imgOriginal+=imgThresholded_line;
@@ -387,6 +595,7 @@ int main( int argc, char** argv )
     imshow("Thresholded Image_line", imgThresholded_line); //show the thresholded image   
     imshow("convexHull",imgHull);
     imshow("region_of_interest",imgField);
+    
 
     waitKey(1); 
   }
